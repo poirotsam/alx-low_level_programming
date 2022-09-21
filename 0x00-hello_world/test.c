b831676b0f5a0ddcf87939731de101f4264c2b0a
Nunayon Samuel <poirot55@gmail.com>
2022-09-20 21:11:29 -0700

    More Functions

---
+++ 0x00-hello_world/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *  *  * main - start point
+ *   *   * run command
+ *    *    * Return: 0
+ *     *    *
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+if a > b
+	{
+		return (a);
+	}
